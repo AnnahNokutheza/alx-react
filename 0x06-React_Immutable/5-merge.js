33ccc18a66db42c1fd8b7910fc1c4062050a2194
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-08-17 20:46:30 +0200

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,11 +1,14 @@
 import { List, Map } from 'immutable';
 
-function concatElements (page1, page2) {
-  return List(page1).concat(List(page2));
+function concatElements(page1, page2) {
+  const list1 = List(page1);
+  const list2 = List(page2);
+  return list1.concat(list2);
 }
 
-function mergeElements (page1, page2) {
-  return Map(page1).merge(page2);
+function mergeElements(page1, page2) {
+  const map1 = Map(page1);
+  return map1.merge(page2);
 }
 
 export {
