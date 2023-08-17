4ee671eabe5e14f644d09421d3f2bfe7a36cfec4
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-08-17 20:30:23 +0200

    Update 1-map.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,16 @@
-import { Map } from 'immutable';
+const { Map } = require('immutable');
 
-export default function getImmutableObject (object) {
-  return Map(object);
+function getImmutableObject(input) {
+  const immutableMap = Map(input);
+  return immutableMap;
 }
+
+const inputObject = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+};
+
+const result = getImmutableObject(inputObject);
+console.log(result);
